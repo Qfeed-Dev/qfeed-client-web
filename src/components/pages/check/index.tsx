3242a0950c8c470003daf8801fea9b70b3247f13
hamo-o <yheel369@gmail.com>
2023-07-27 10:59:38 +0900

    feat: design system

--- src/components/pages/check/index.tsx
+++ src/components/pages/check/index.tsx
@@ -1,10 +1,18 @@
 'use client';
 import { styled } from 'styled-components';
+import { Text } from 'src/components/common/Text';
 
 const Hi = () => {
-  return <Test>check page</Test>;
+  return (
+    <Text typo="Headline1b" color="primary_qgreen">
+      테스트 페이지입니다.
+    </Text>
+  );
 };
 
-const Test = styled.div``;
+const Test = styled.div`
+  width: 100%;
+  height: 2rem;
+`;
 
 export default Hi;
