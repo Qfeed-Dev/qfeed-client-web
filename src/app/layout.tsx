cb87dbf990f10b497792874338197a3a411540ae
hamo-o <yheel369@gmail.com>
2023-07-30 17:04:10 +0900

    feat: edit layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,19 @@
-import StyledComponentsRegistry from '../lib/registry';
-import '../../styles/globals.css';
-import Layout from 'src/components/layout/Layout';
+import StyledComponentsRegistry from "../lib/registry";
+import "../../styles/globals.css";
+import Layout from "src/components/layout/Layout";
 
 export default function RootLayout({
-  children,
+    children
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html>
-      <body>
-        <StyledComponentsRegistry>
-          <Layout>{children}</Layout>
-        </StyledComponentsRegistry>
-      </body>
-    </html>
-  );
+    return (
+        <html>
+            <body style={{ background: "#131313" }}>
+                <StyledComponentsRegistry>
+                    <Layout>{children}</Layout>
+                </StyledComponentsRegistry>
+            </body>
+        </html>
+    );
 }
