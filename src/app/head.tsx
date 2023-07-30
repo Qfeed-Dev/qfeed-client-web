0a3ed1f7df2e8e3ec7cc8f3fa5924bf41b1df112
hamo-o <yheel369@gmail.com>
2023-07-30 16:21:48 +0900

    design: 웹뷰를 위한 디자인 세팅 추가

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,8 +1,11 @@
 export default function Head() {
-  return (
-    <>
-      <title>My Page Title</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </>
-  );
+    return (
+        <>
+            <title>My Page Title</title>
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1, user-scalable=0"
+            />
+        </>
+    );
 }
