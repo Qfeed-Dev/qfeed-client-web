8ed1a02c897d7af30307b547a07f745e83e83da9
hamo-o <yheel369@gmail.com>
2023-08-02 21:31:41 +0900

    feat: add navigation component

---
+++ src/components/navigations/navigation-top.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { styled } from "styled-components";
+import Flex from "src/components/common/Flex";
+import Text from "../common/Text";
+
+export interface NavProps {
+    title: string;
+    leftIcon?: React.ReactNode;
+    rightIcon?: React.ReactNode;
+}
+
+const NavigationTop = ({ title, leftIcon, rightIcon }: NavProps) => {
+    return (
+        <NavWrapper>
+            {leftIcon}
+            <Text typo="Headline1b">{title}</Text>
+            {rightIcon}
+        </NavWrapper>
+    );
+};
+
+const NavWrapper = styled.div`
+    width: 100%;
+    padding: 0.62rem 0;
+
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+`;
+
+export default NavigationTop;
