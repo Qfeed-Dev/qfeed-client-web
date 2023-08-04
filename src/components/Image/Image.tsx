f43aae9df83e314fb77ebb5b59ea7496e2c672d2
cyeju <yeju1019@gmail.com>
2023-08-04 12:35:08 +0900

    fix: home 페이지에 pinterest 추가

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -3,30 +3,47 @@ import { styled } from "styled-components";
 import { match } from "ts-pattern";
 
 interface Props {
-  src: any;
-  type: string;
-  size?: number;
+    src: any;
+    type: string;
+    size?: number;
+    height?: number;
 }
 
-const Image = ({ src, type = "profile", size }: Props) => {
-  return (
-    <ImageWrapper
-      src={src}
-      size={match(type)
-        .with("profile", () => 40)
-        .with("friend", () => 35)
-        .with("default", () => size)
-        .exhaustive()}
-    />
-  );
+const Image = ({ src, type = "profile", size, height }: Props) => {
+    return (
+        <ImageWrapper
+            src={src}
+            width={match(type)
+                .with("home", () => "100%")
+                .with("default", () => size)
+                .exhaustive()}
+            height={match(type)
+                .with("home", () => (height ? height : "100%"))
+                .with("default", () => size)
+                .exhaustive()}
+            ratio={match(type)
+                .with("home", () => "auto")
+                .with("default", () => 1)
+                .exhaustive()}
+            radius={match(type)
+                .with("home", () => "10px")
+                .with("default", () => "999px")
+                .exhaustive()}
+        />
+    );
 };
 
-const ImageWrapper = styled.img<{ size: number }>`
-  width: ${({ size }) => size + "px"};
-  height: ${({ size }) => size + "px"};
-  margin: auto;
+const ImageWrapper = styled.img<{
+    ratio: number;
+    radius: number;
+}>`
+    margin: auto;
 
-  border-radius: 50%;
+    border-radius: 50%;
+
+    aspect-ratio: ${({ ratio }) => ratio};
+    border-radius: ${({ radius }) => radius};
+    object-fit: cover;
 `;
 
 export default Image;
--- src/components/pages/home/QfeedFrame.tsx
+++ src/components/pages/home/QfeedFrame.tsx
@@ -1,34 +1,72 @@
 "use client";
-import { colors } from "src/constants/colors";
+import { Text } from "src/components/common/Text";
+import Image from "src/components/Image";
+import Spacing from "src/components/Spacing";
 import useDisplaySize from "src/hooks/useDisplaySize";
-import { styled } from "styled-components";
-import { match } from "ts-pattern";
+import styled from "styled-components";
+import { colors, repeatColor, typo } from "styles/theme";
 
 interface Props {
-  onClick: () => void;
+    idx: number;
+    data: any;
+    onClick: () => void;
 }
 
-const QfeedFrame = ({ onClick }: Props) => {
-  const { width } = useDisplaySize();
-  return (
-    <QfeedFrameWrapper onClick={onClick} width={(width - 16 * 2 - 12) / 2}>
-      Hi
-    </QfeedFrameWrapper>
-  );
+const QfeedFrame = ({ idx, data, onClick }: Props) => {
+    const { width } = useDisplaySize();
+
+    return (
+        <QfeedFrameWrapper onClick={onClick} repeatColor={repeatColor[idx]}>
+            <div style={{ padding: 3, overflow: "hidden" }}>
+                {/* <ImageWrapper>
+                    <Image
+                        type="home"
+                        src="https://i.ibb.co/0Z6FNN7/60pt.png"
+                    />
+                </ImageWrapper> */}
+                <QfeedFrameInner>
+                    <Text typo="Headline2b" color="light_qwhite">
+                        {data.title}
+                    </Text>
+                    <Spacing size={4} />
+                    <Text typo="Caption1r" color="light_qwhite">
+                        {data.title}
+                    </Text>
+                    <Spacing size={27} />
+                    <CountWrapper>
+                        <div style={{ margin: "auto 0" }}>
+                            <Text typo="Subtitle1b" color="light_qwhite">
+                                {data.answer}명 응답
+                            </Text>
+                        </div>
+                    </CountWrapper>
+                </QfeedFrameInner>
+            </div>
+        </QfeedFrameWrapper>
+    );
 };
 
-const QfeedFrameWrapper = styled.div<{ width: number }>`
-  width: ${({ width }) => width + "px"};
-  height: 20px;
-
-  display: flex;
-  gap: 12px;
-  border-radius: 10px;
-  background-color: yellowgreen;
+const QfeedFrameWrapper = styled.div<{ repeatColor: any }>`
+    height: calc(100% + 20px);
+    // position: relative;
+    border-radius: 10px;
+    background-color: ${({ repeatColor }) => repeatColor};
 `;
 
-const Menu = styled.div`
-  color: ${colors.Qwhite};
+const QfeedFrameInner = styled.div`
+    padding: 28px 20px;
+    overflow: hidden;
+    border-radius: 10px;
+    background-color: black;
+`;
+
+const ImageWrapper = styled.div`
+    position: absolute;
+`;
+
+const CountWrapper = styled.div`
+    height: 36px;
+    display: flex;
 `;
 
 export default QfeedFrame;
