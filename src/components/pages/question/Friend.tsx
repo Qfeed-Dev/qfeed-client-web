1cce06154e7fbde960edee4c4a058c945bb4ca73
cyeju <yeju1019@gmail.com>
2023-08-04 12:35:08 +0900

    fix: home 페이지에 pinterest 추가

--- src/components/pages/question/Friend.tsx
+++ src/components/pages/question/Friend.tsx
@@ -8,54 +8,58 @@ import { changeVisibleType } from "src/reducer/slices/bottomSheet/bottomSheetSli
 import { styled } from "styled-components";
 
 export default function Friend() {
-  const { width } = useDisplaySize();
-  const dispatch = useAppDispatch();
+    const { width } = useDisplaySize();
+    const dispatch = useAppDispatch();
 
-  const handleClickFriend = () => {
-    dispatch(
-      changeVisibleType({
-        type: "bottomSheet",
-        value: [1, "friend"],
-      })
+    const handleClickFriend = () => {
+        dispatch(
+            changeVisibleType({
+                type: "bottomSheet",
+                value: [1, "friend"]
+            })
+        );
+    };
+
+    return (
+        <FriendWrapper
+            width={(width - 16 * 2 - 12 * 3) / 4}
+            onClick={handleClickFriend}
+        >
+            <FriendInner>
+                <Menu>
+                    <Image
+                        type="default"
+                        size={35}
+                        src="https://i.ibb.co/0Z6FNN7/60pt.png"
+                    />
+                </Menu>
+                <Spacing size={8} />
+
+                <Menu>
+                    <Name>김은별</Name>
+                </Menu>
+                <Menu>
+                    <QfeedId>dlraud1</QfeedId>
+                </Menu>
+            </FriendInner>
+        </FriendWrapper>
     );
-  };
-
-  return (
-    <FriendWrapper
-      width={(width - 16 * 2 - 12 * 3) / 4}
-      onClick={handleClickFriend}
-    >
-      <FriendInner>
-        <Menu>
-          <Image src="https://i.ibb.co/0Z6FNN7/60pt.png" type="friend" />
-        </Menu>
-        <Spacing size={8} />
-
-        <Menu>
-          <Name>김은별</Name>
-        </Menu>
-        <Menu>
-          <QfeedId>dlraud1</QfeedId>
-        </Menu>
-      </FriendInner>
-    </FriendWrapper>
-  );
 }
 
 const FriendWrapper = styled.div<{ width: number }>`
-  width: ${({ width }) => width + "px"};
-  //   height: ${({ width }) => (width * 92) / 73 + "px"};
+    width: ${({ width }) => width + "px"};
+    //   height: ${({ width }) => (width * 92) / 73 + "px"};
 
-  border-radius: 10px;
-  background-color: ${colors.Qorange};
+    border-radius: 10px;
+    background-color: ${colors.Qorange};
 `;
 
 const FriendInner = styled.div`
-  padding: 8px 20px;
+    padding: 8px 20px;
 `;
 
 const Menu = styled.div`
-  text-align: center;
+    text-align: center;
 `;
 
 const Name = styled.div``;
--- styles/theme/colors.tsx
+++ styles/theme/colors.tsx
@@ -1,32 +1,48 @@
 const hexToRGBA = (color: string, opacity: number) => {
-  const r = parseInt(color.slice(1, 3), 16);
-  const g = parseInt(color.slice(3, 5), 16);
-  const b = parseInt(color.slice(5, 7), 16);
+    const r = parseInt(color.slice(1, 3), 16);
+    const g = parseInt(color.slice(3, 5), 16);
+    const b = parseInt(color.slice(5, 7), 16);
 
-  return `rgba(${r}, ${g}, ${b}, ${opacity * 0.01})`;
+    return `rgba(${r}, ${g}, ${b}, ${opacity * 0.01})`;
 };
 
 export const colors = {
-  primary_qyellow: '#EEF619',
-  primary_qpink: '#FE98CA',
-  primary_qorange: '#FF812F',
-  primary_qred: '#FF5B5B',
-  primary_qblue: '#0167F7',
-  primary_qgreen: '#28D3A7',
+    primary_qyellow: "#EEF619",
+    primary_qpink: "#FE98CA",
+    primary_qorange: "#FF812F",
+    primary_qred: "#FF5B5B",
+    primary_qblue: "#0167F7",
+    primary_qgreen: "#28D3A7",
 
-  light_qblack: '#131313',
-  light_qwhite: '#FFFFFF',
-  light_gray0: '#D9D9D9',
-  light_gray1: '#BDBDBD',
-  light_gray2: '#757575',
-  light_gray3: '#484848',
+    light_qblack: "#131313",
+    light_qwhite: "#FFFFFF",
+    light_gray0: "#D9D9D9",
+    light_gray1: "#BDBDBD",
+    light_gray2: "#757575",
+    light_gray3: "#484848",
 
-  line_black_70: hexToRGBA('#131313', 70),
-  line_black_50: hexToRGBA('#131313', 50),
-  line_black_30: hexToRGBA('#131313', 30),
-  line_black_5: hexToRGBA('#131313', 5),
-  line_white_70: hexToRGBA('#FFFFFF', 70),
-  line_white_50: hexToRGBA('#FFFFFF', 50),
-  line_white_30: hexToRGBA('#FFFFFF', 30),
-  line_white_5: hexToRGBA('#FFFFFF', 5),
+    line_black_70: hexToRGBA("#131313", 70),
+    line_black_50: hexToRGBA("#131313", 50),
+    line_black_30: hexToRGBA("#131313", 30),
+    line_black_5: hexToRGBA("#131313", 5),
+    line_white_70: hexToRGBA("#FFFFFF", 70),
+    line_white_50: hexToRGBA("#FFFFFF", 50),
+    line_white_30: hexToRGBA("#FFFFFF", 30),
+    line_white_5: hexToRGBA("#FFFFFF", 5)
 };
+
+// 12 개
+export const repeatColor = [
+    colors.primary_qpink,
+    colors.primary_qorange,
+    colors.primary_qblue,
+    colors.primary_qyellow,
+    colors.primary_qyellow,
+    colors.primary_qgreen,
+    colors.primary_qpink,
+    colors.primary_qred,
+    colors.primary_qorange,
+    colors.primary_qblue,
+    colors.primary_qred,
+    colors.primary_qgreen
+];
