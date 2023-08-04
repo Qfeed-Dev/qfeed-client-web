d64107245e6be5add87042c1b3fe3788b5b5dcd4
cyeju <yeju1019@gmail.com>
2023-08-04 19:08:57 +0900

    fix: home 페이지 list 수정

--- src/components/pages/home/QfeedFrame.tsx
+++ src/components/pages/home/QfeedFrame.tsx
@@ -14,6 +14,8 @@ interface Props {
 
 const QfeedFrame = ({ idx, data, onClick }: Props) => {
     const { width } = useDisplaySize();
+    const imageUrl = "https://i.ibb.co/0Z6FNN7/60pt.png";
+    // const imageUrl = null;
 
     return (
         <QfeedFrameWrapper
@@ -21,24 +23,39 @@ const QfeedFrame = ({ idx, data, onClick }: Props) => {
             repeatBackgroundColor={repeatBackgroundColor[idx]}
         >
             <div style={{ padding: 3, overflow: "hidden" }}>
-                <ImageWrapper>
+                {/* <ImageWrapper>
                     <Image
                         type="home"
                         src="https://i.ibb.co/0Z6FNN7/60pt.png"
+                        // height={"100%"}
                     />
-                </ImageWrapper>
-                <QfeedFrameInner>
-                    <Text typo="Headline2b" color="light_qwhite">
+                </ImageWrapper> */}
+                <QfeedFrameInner
+                    imageUrl={imageUrl}
+                    backgroundColor={repeatBackgroundColor[idx]}
+                >
+                    <Text
+                        typo="Headline2b"
+                        color={imageUrl ? "light_qwhite" : "light_qblack"}
+                    >
                         {data.title}
                     </Text>
                     <Spacing size={4} />
-                    <Text typo="Caption1r" color="light_qwhite">
+                    <Text
+                        typo="Caption1r"
+                        color={imageUrl ? "light_qwhite" : "light_qblack"}
+                    >
                         {data.title}
                     </Text>
                     <Spacing size={27} />
                     <CountWrapper>
                         <div style={{ margin: "auto 0" }}>
-                            <Text typo="Subtitle1b" color="light_qwhite">
+                            <Text
+                                typo="Subtitle1b"
+                                color={
+                                    imageUrl ? "light_qwhite" : "light_qblack"
+                                }
+                            >
                                 {data.answer}명 응답
                             </Text>
                         </div>
@@ -56,20 +73,50 @@ const QfeedFrameWrapper = styled.div<{ repeatBackgroundColor: any }>`
     background-color: ${({ repeatBackgroundColor }) => repeatBackgroundColor};
 `;
 
-const QfeedFrameInner = styled.div`
+const QfeedFrameInner = styled.div<{ imageUrl: any; backgroundColor: any }>`
     padding: 28px 20px;
     overflow: hidden;
-    border-radius: 10px;
-    background-color: black;
-`;
 
-const ImageWrapper = styled.div`
-    position: absolute;
+    border-radius: 10px;
+    color: ${({ imageUrl }) =>
+        imageUrl ? colors.light_qwhite : colors.light_qblack};
+    background-color: ${({ imageUrl }) =>
+        imageUrl ? colors.light_qblack : null};
+
+    &::before {
+        content: "";
+        border-radius: 10px;
+        background-color: ${({ imageUrl, backgroundColor }) =>
+            imageUrl ? null : backgroundColor};
+        background-image: url(${({ imageUrl }) => imageUrl});
+        background-size: cover;
+        background-position: center;
+        opacity: 0.5;
+        position: absolute;
+        top: 3px;
+        left: 3px;
+        right: 3px;
+        bottom: 3px;
+    }
+
+    div {
+        position: relative;
+    }
 `;
 
 const CountWrapper = styled.div`
     height: 36px;
     display: flex;
+`;
+
+const Background = styled.div`
+    width: 100%;
+    height: 100%;
+    margin-right: 6px;
+
+    opacity: 0.5;
+    position: absolute;
+    background-color: ${colors.light_qwhite};
 `;
 
 export default QfeedFrame;
