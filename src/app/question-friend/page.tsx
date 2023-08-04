d37362d9a94f03a6c21f17c688822b64fa56b50d
cyeju <yeju1019@gmail.com>
2023-08-04 14:08:05 +0900

    fix: question-friend 페이지 전체적인 수정

--- src/app/question-friend/page.tsx
+++ src/app/question-friend/page.tsx
@@ -11,81 +11,81 @@ import { globalValue } from "src/constants/globalValue";
 import { styled } from "styled-components";
 
 const QuestionDatas = [
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {}
 ];
 
 export default function Page() {
-  return (
-    <>
-      <QuestionWrapper>
-        <BackTitle />
+    return (
+        <>
+            <QuestionWrapper>
+                <BackTitle />
 
-        <SlideLine />
-        <Spacing size={50} />
-        <Question />
-        <Spacing size={38} />
+                <SlideLine />
+                <Spacing size={50} />
+                <Question />
+                <Spacing size={38} />
 
-        <Input type="question-friend" />
-        <Spacing size={16} />
+                <Input type="question-friend" />
+                <Spacing size={16} />
 
-        <FriendWrapper>
-          {QuestionDatas.map((data: any, idx: number) => {
-            return <Friend key={idx} />;
-          })}
-        </FriendWrapper>
-        <Spacing size={111 + 20} />
-      </QuestionWrapper>
+                <FriendWrapper>
+                    {QuestionDatas.map((data: any, idx: number) => {
+                        return <Friend key={idx} idx={idx} />;
+                    })}
+                </FriendWrapper>
+                <Spacing size={111 + 20} />
+            </QuestionWrapper>
 
-      <BottomButton>
-        <BottomInner>
-          <Button type="secondary">넘기기</Button>
-          <Button type="primary">보내기</Button>
-        </BottomInner>
-      </BottomButton>
-    </>
-  );
+            <BottomButton>
+                <BottomInner>
+                    <Button type="secondary">넘기기</Button>
+                    <Button type="primary">보내기</Button>
+                </BottomInner>
+            </BottomButton>
+        </>
+    );
 }
 
 const QuestionWrapper = styled.div`
-  height: 100%;
-  padding: 0 16px;
+    height: 100%;
+    padding: 0 16px;
 `;
 
 const FriendWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  gap: 14px 12px;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 14px 12px;
 `;
 
 const BottomButton = styled.div`
-  width: 100%;
-  height: 131px;
+    width: 100%;
+    height: 131px;
 
-  position: absolute;
-  left: 0;
-  bottom: 0;
+    position: absolute;
+    left: 0;
+    bottom: 0;
 
-  background-color: ${colors.Qblack};
+    background-color: ${colors.Qblack};
 `;
 
 const BottomInner = styled.div`
-  margin-top: 20px;
-  padding: 0 16px;
-  display: flex;
-  gap: 14px;
+    margin-top: 20px;
+    padding: 0 16px;
+    display: flex;
+    gap: 14px;
 `;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,48 +3,47 @@ import { forwardRef } from "react";
 import { colors } from "src/constants/colors";
 import { styled } from "styled-components";
 import { match } from "ts-pattern";
+import { Text } from "../common/Text";
 
 interface Props {
-  type: "primary" | "secondary" | "default";
-  children?: any;
+    type: "primary" | "secondary" | "default";
+    children?: any;
 }
 
 const Button = forwardRef(function Button(
-  { children, type = "primary", ...props }: Props,
-  ref
+    { children, type = "primary", ...props }: Props,
+    ref
 ) {
-  return (
-    <ButtonWrapper
-      color={match(type)
-        .with("primary", () => colors.Qblack)
-        .with("secondary", () => colors.Qwhite)
-        .exhaustive()}
-      backgroundColor={match(type)
-        .with("primary", () => colors.Qwhite)
-        .with("secondary", () => colors.Qblack)
-        .exhaustive()}
-    >
-      <Text>{children}</Text>
-    </ButtonWrapper>
-  );
+    return (
+        <ButtonWrapper
+            color={match(type)
+                .with("primary", () => colors.Qblack)
+                .with("secondary", () => colors.Qwhite)
+                .exhaustive()}
+            backgroundColor={match(type)
+                .with("primary", () => colors.Qwhite)
+                .with("secondary", () => colors.Qblack)
+                .exhaustive()}
+        >
+            <Text typo="Subtitle1b" style={{ margin: "auto" }}>
+                {children}
+            </Text>
+        </ButtonWrapper>
+    );
 });
 
 const ButtonWrapper = styled.div<{
-  color: any;
-  backgroundColor: any;
+    color: any;
+    backgroundColor: any;
 }>`
-  width: 100%;
-  height: 47px;
-  display: flex;
+    width: 100%;
+    height: 47px;
+    display: flex;
 
-  color: ${({ color }) => color};
-  border: 1px solid ${colors.Qwhite};
-  background-color: ${({ backgroundColor }) => backgroundColor};
-`;
-
-const Text = styled.div`
-  margin: auto;
-  display: flex;
+    color: ${({ color }) => color};
+    border: 1px solid ${colors.Qwhite};
+    border-radius: 10px;
+    background-color: ${({ backgroundColor }) => backgroundColor};
 `;
 
 export default Button;
