7543ab9fcd4b44710335ae1c45b31954c14a642a
hamo-o <yheel369@gmail.com>
2023-08-05 14:54:53 +0900

    rename: kakao redirect uri 폴더 이동

--- src/components/pages/login/index.tsx
+++ src/components/pages/login/index.tsx
@@ -2,12 +2,12 @@
 import Link from "next/link";
 import Flex from "src/components/common/Flex";
 import Text from "src/components/common/Text";
+import { useTest } from "src/hooks/common/useTest";
 
 const KaKaoLogin = () => {
     return (
         <Flex direction="column" gap={24}>
             <Text typo="Headline2b">로그인</Text>
-
             <Link
                 href={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`}
             >
