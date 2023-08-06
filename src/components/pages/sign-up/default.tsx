3beee0f17d7f463d46d241a6a435ace05d0c9d94
hamo-o <yheel369@gmail.com>
2023-08-06 15:22:02 +0900

    feat: 닉네임 중복체크 및 유효성 메시지

--- src/components/pages/sign-up/default.tsx
+++ src/components/pages/sign-up/default.tsx
@@ -1,11 +1,19 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import ButtonFillLarge from "src/components/buttons/button-fill-large";
-import ButtonFillSmall from "src/components/buttons/button-fill-small";
 import Flex from "src/components/common/Flex";
 import InputLine from "src/components/inputs/input-line";
 import NavigationTop from "src/components/navigations/navigation-top";
 import ButtonGenderSelect from "src/components/sign-up/button-gender-select";
+import {
+    birthMsg,
+    emailMsg,
+    nameMsg,
+    nicknameMsg,
+    phoneMsg
+} from "src/constants/messages";
+import { useCheckNicknameQuery } from "src/hooks/account/useCheckNicknameQuery";
 import { useInput } from "src/hooks/common/useInput";
 import { useToggle } from "src/hooks/sign-up/useToggle";
 
@@ -20,14 +28,10 @@ const SignIn = () => {
 
     const gender = useToggle("여성");
 
-    console.log({
-        name: name.value,
-        gender: gender.value,
-        birthday: birthday.value,
-        phone: phone.value,
-        email: email.value,
-        nickname: nickname.value
-    });
+    const isDupNickname = useCheckNicknameQuery(nickname.value);
+    useEffect(() => {
+        isDupNickname.refetch();
+    }, [nickname.value]);
 
     return (
         <Flex direction="column" justify="start" gap={24}>
@@ -41,6 +45,7 @@ const SignIn = () => {
                     onChange={name.handleChangeInput}
                     label="이름"
                     placeholder="ex) 황채린"
+                    message={nameMsg.RIGHT}
                 />
                 <ButtonGenderSelect
                     value={gender.value}
@@ -51,18 +56,21 @@ const SignIn = () => {
                     onChange={birthday.handleChangeInput}
                     label="생년월일"
                     placeholder="ex) 1997.04.02"
+                    message={birthMsg.RIGHT}
                 />
                 <InputLine
                     value={phone.value}
                     onChange={phone.handleChangeInput}
                     label="휴대폰 번호"
                     placeholder="ex) 010-5016-5886"
+                    message={phoneMsg.RIGHT}
                 />
                 <InputLine
                     value={email.value}
                     onChange={email.handleChangeInput}
                     label="이메일"
                     placeholder="ex) ghkdcofls42@naver.com"
+                    message={emailMsg.RIGHT}
                 />
                 <Flex align="end" gap={12}>
                     <InputLine
@@ -70,8 +78,12 @@ const SignIn = () => {
                         onChange={nickname.handleChangeInput}
                         label="닉네임"
                         placeholder="ex) qwerk11"
+                        message={
+                            isDupNickname.data?.abailable
+                                ? nicknameMsg.RIGHT
+                                : nicknameMsg.DUPLICATE
+                        }
                     />
-                    <ButtonFillSmall text="중복 확인" onClick={() => {}} />
                 </Flex>
                 <ButtonFillLarge
                     state="disabled"
---
+++ src/constants/messages.ts
@@ -0,0 +1,25 @@
+export const nicknameMsg = {
+    RIGHT: "사용 가능한 닉네임입니다.",
+    WRONG: "영어, 숫자, _ 만 사용 가능합니다.",
+    DUPLICATE: "중복된 닉네임입니다."
+};
+
+export const nameMsg = {
+    RIGHT: "사용 가능한 이름입니다.",
+    WRONG: "영어, 숫자, _ 만 사용 가능합니다."
+};
+
+export const birthMsg = {
+    RIGHT: "사용 가능한 생년월일입니다.",
+    WRONG: "영어, 숫자, _ 만 사용 가능합니다."
+};
+
+export const phoneMsg = {
+    RIGHT: "사용 가능한 휴대폰 번호입니다.",
+    WRONG: "영어, 숫자, _ 만 사용 가능합니다."
+};
+
+export const emailMsg = {
+    RIGHT: "사용 가능한 이메일입니다.",
+    WRONG: "영어, 숫자, _ 만 사용 가능합니다."
+};
