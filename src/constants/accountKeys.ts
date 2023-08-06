29927e8b4cb5d819a10788eff11bf11492a0e9e6
hamo-o <yheel369@gmail.com>
2023-08-06 15:47:05 +0900

    feat: 유효성에 따른 button state

--- src/constants/accountKeys.ts
+++ src/constants/accountKeys.ts
@@ -2,3 +2,8 @@ export const nicknameKeys = {
     all: ["nickname"] as const
     // detail: (id: number) => [...userRewardKeys.all, id] as const
 };
+
+export const userKeys = {
+    all: ["user"] as const
+    // detail: (id: number) => [...userRewardKeys.all, id] as const
+};
