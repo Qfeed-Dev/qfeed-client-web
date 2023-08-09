091a46cf4af58fed6e4f0c9942165cd6e45d6bf8
cyeju <yeju1019@gmail.com>
2023-08-09 19:53:15 +0900

    fix: home

--- src/hooks/useDisplaySize.ts
+++ src/hooks/useDisplaySize.ts
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 
 export default function useDisplaySize() {
-  const [width, setWidth] = useState<number>(0);
-  const [height, setHeight] = useState<number>(0);
+    const [width, setWidth] = useState<number>(0);
+    const [height, setHeight] = useState<number>(0);
 
-  useEffect(() => {
-    setWidth(window.innerWidth);
-    setHeight(window.innerHeight);
-  }, []);
+    useEffect(() => {
+        setWidth(window.innerWidth > 600 ? 600 : window.innerWidth);
+        setHeight(window.innerHeight);
+    }, []);
 
-  return { width, height };
+    return { width, height };
 }
