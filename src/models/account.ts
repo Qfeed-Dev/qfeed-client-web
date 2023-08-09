8a220d69dc5b8b8de3da29f271fcedd7e614e8e7
hamo-o <yheel369@gmail.com>
2023-08-09 21:08:20 +0900

    refactor: User interface 분리

---
+++ src/models/account.ts
@@ -0,0 +1,13 @@
+export interface User {
+    id: number;
+    email: string;
+    nickname: string;
+    schoolType: string;
+    schoolName: string;
+    grade: string;
+    class: string;
+    gender: string;
+    birthday: string;
+    profileImage: string;
+    idCardImage: string;
+}
