fe78fe3fda47c9656c6f7b9a59b9db581ec4f899
hamo-o <yheel369@gmail.com>
2023-08-10 16:31:44 +0900

    chore: add react query devtools

--- src/lib/react-query-registry.tsx
+++ src/lib/react-query-registry.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PropsWithChildren, useState } from "react";
 
 export default function ReactQueryProvider({ children }: PropsWithChildren) {
@@ -9,6 +10,7 @@ export default function ReactQueryProvider({ children }: PropsWithChildren) {
     return (
         <QueryClientProvider client={queryClient}>
             {children}
+            <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     );
 }
