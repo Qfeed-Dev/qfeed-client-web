5796d0f7bae368a70f31cfde2fa22670ae40f5f7
hamo-o <yheel369@gmail.com>
2023-08-12 16:09:08 +0900

    feat: 401 error 처리

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,4 +1,6 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance, AxiosError } from "axios";
+import { cookies } from "next/headers";
+import { deleteCookie } from "src/utils/cookie";
 
 export const qFeedAxios: AxiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -6,3 +8,26 @@ export const qFeedAxios: AxiosInstance = axios.create({
         "Content-Type": "application/json;charset=utf-8"
     }
 });
+
+qFeedAxios.interceptors.response.use(
+    (response) => {
+        if (
+            !(
+                response.status === 200 ||
+                response.status === 201 ||
+                response.status === 204
+            )
+        )
+            throw new Error();
+        if (response.data.errors) throw new Error(response.data.errors);
+        return response;
+    },
+    async (error) => {
+        const err = error as AxiosError;
+        if (err.response?.status === 401) {
+            deleteCookie();
+            window.location.href = "/account";
+        }
+        return Promise.reject(error);
+    }
+);
