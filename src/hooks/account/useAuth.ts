d3ef530fedcb390b0e15ec03ed8f07239059dec8
이현영 <89445100+hamo-o@users.noreply.github.com>
2023-08-13 02:42:45 +0900

    Revert "[Fix] 배포환경에서만 로그인 이후 라우팅 문제 발생 해결"

--- src/hooks/account/useAuth.ts
+++ src/hooks/account/useAuth.ts
@@ -18,6 +18,7 @@ const getAccessToken = async (code: string) => {
 
 export const useAuth = () => {
     const router = useRouter();
+    const { user, isLoading } = useUserQuery();
     const [isActive, setIsActive] = useState<boolean | undefined>(undefined);
 
     const kakaoMutation = useMutation(getAccessToken, {
@@ -29,13 +30,12 @@ export const useAuth = () => {
         }
     });
 
-    const { user, isLoading } = useUserQuery();
     useEffect(() => {
         if (!kakaoMutation.isLoading && !isLoading && user !== undefined) {
             setIsActive(useIsActive(user));
             isActive ? router.push("/") : router.push("/sign-up/default");
         }
-    }, [kakaoMutation.isLoading, isActive, router]);
+    }, [kakaoMutation.isLoading, isLoading, user, isActive, router]);
 
     return {
         kakaoMutation
