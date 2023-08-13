17220f8ccd8d81d0e2e124897eae41e1b4d653fb
hamo-o <yheel369@gmail.com>
2023-08-14 00:51:39 +0900

    fix: User interface 변경

--- src/models/account.ts
+++ src/models/account.ts
@@ -1,13 +1,15 @@
 export interface User {
-    id: number;
-    email: string;
-    nickname: string;
-    schoolType: string;
-    schoolName: string;
-    grade: string;
-    class: string;
-    gender: string;
-    birthday: string;
-    profileImage: string;
-    idCardImage: string;
+    id?: number;
+    name?: string;
+    email?: string;
+    phone?: string;
+    nickname?: string;
+    schoolType?: string;
+    schoolName?: string;
+    grade?: string;
+    class?: string;
+    gender?: string;
+    birthday?: string;
+    profileImage?: string;
+    idCardImage?: string;
 }
