18ce6663d7ff16dbcf600e9a9cc01a4ae87bc840
hamo-o <yheel369@gmail.com>
2023-08-20 10:28:27 +0900

    feat: 나를 선택한 큐피드

--- src/pages-edit/mypage/components/QfeedList.tsx
+++ src/pages-edit/mypage/components/QfeedList.tsx
@@ -1,26 +1,32 @@
 "use client";
 
 import styled from "styled-components";
-import { colors } from "styles/theme";
 
-import Flex from "src/components/common/Flex";
-import Text from "src/components/common/Text";
-
-import { useUserQuery } from "src/hooks/account/useUserQuery";
 import { useToggle } from "src/hooks/common/useToggle";
 
+import Flex from "src/components/common/Flex";
 import ButtonQfeedSelect from "./ButtonQfeedSelect";
-import QfeedFrame from "src/pages-edit/home/components/QfeedFrame";
+import MakeList from "./MakeList";
+import SelectList from "./SelectList";
+
+import { globalValue } from "src/constants/globalValue";
 
 export default function QfeedList() {
-    const { user } = useUserQuery();
     const { value, handleChangeState } = useToggle("내가 만든 큐피드");
+
     return (
-        <>
+        <Flex direction="column" gap={16}>
             <ButtonQfeedSelect value={value} onClick={handleChangeState} />
-            {/* {HomeDatas.map((data: any, idx: number) => 
-            <QfeedFrame />
-            } */}
-        </>
+            {value === "내가 만든 큐피드" ? <MakeList /> : <SelectList />}
+        </Flex>
     );
 }
+
+const QfeedWrapper = styled.div`
+    width: 100%;
+    max-width: 600px;
+    height: calc(100% - ${globalValue.bottomSheetHeight});
+
+    margin: 0 auto;
+    position: relative;
+`;
