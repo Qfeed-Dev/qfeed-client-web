3a8d171fb7e9e9eaa358871f83cf6510bbd264de
hamo-o <yheel369@gmail.com>
2023-08-25 10:42:22 +0900

    feat: modal 구현

---
+++ src/components/common/Backdrop.tsx
@@ -0,0 +1,39 @@
+import { motion } from "framer-motion";
+import React from "react";
+import styled from "styled-components";
+import { colors } from "styles/theme";
+
+const Backdrop = ({
+    children,
+    onClick
+}: {
+    children: React.ReactNode;
+    onClick: any;
+}) => {
+    return (
+        <BackdropWrapper
+            onClick={onClick}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+        >
+            {children}
+        </BackdropWrapper>
+    );
+};
+
+const BackdropWrapper = styled(motion.div)`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background: ${colors.line_black_50};
+`;
+
+export default Backdrop;
