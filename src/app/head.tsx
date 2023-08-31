6237d5a39b96646e42bf2c3ef9fd87dc689f8fd0
hamo-o <yheel369@gmail.com>
2023-08-31 18:23:36 +0900

    feat: 확대 막기

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
             <title>Q-feed</title>
             <meta
                 name="viewport"
-                content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1, user-scalable=0"
+                content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
             />
         </>
     );
