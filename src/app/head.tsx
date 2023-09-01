b4fd4f8c2e4a8e2f5a93189980f05c95c1a10e74
hamo-o <yheel369@gmail.com>
2023-09-01 21:08:20 +0900

    fix: globalStyle 에러 수정

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -6,11 +6,6 @@ export default function Head() {
                 name="viewport"
                 content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"
             />
-            <link
-                rel="stylesheet"
-                type="text/css"
-                href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
-            />
         </>
     );
 }
