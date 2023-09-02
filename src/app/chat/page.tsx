bf2b6748b1046199193719c40fc8e66f160cbdaf
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-09-02 09:42:11 +0900

    add chatting routes

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -3,16 +3,55 @@ import BottomNavigation from "src/components/BottomNavigation";
 import ChatTitle from "src/pages-edit/chat/ChatTitle";
 import styled from "styled-components";
 import Chatting from "src/pages-edit/chat/Chatting";
+import { useGetChatrooms } from "src/hooks/chatting/useGetChatrooms";
+import Loading from "src/components/common/Loading";
+import FriendList from "src/pages-edit/chatting/FriendsList";
+import Text from "src/components/common/Text";
+import Spacing from "src/components/Spacing/Spacing";
+import Button from "src/components/Button/Button";
+import { useState } from "react";
 
 export default function Page() {
+    const { data, isLoading } = useGetChatrooms();
+
+    const [showFriendsList, setShowFriendsList] = useState(false);
+    const toggleShowFriendsList = () => setShowFriendsList(!showFriendsList);
+
+    // const { selectedIdx } = useAppSelector((state) => state.bottomSheet);
+
+    // const { data, isLoading } = useGetChatroomChats(selectedIdx);
+
     return (
         <>
             <ChatWrapper>
                 <ChatTitle />
+                <Spacing size={16} />
                 <ChattingWrapper>
-                    {chats.map((data: any, idx: number) => {
-                        return <Chatting key={idx} />;
-                    })}
+                    {isLoading ? (
+                        <Loading />
+                    ) : data?.data?.length ? (
+                        <>
+                            {data?.data?.map((data: any, idx: number) => {
+                                return <Chatting key={idx} {...data} />;
+                            })}
+                            <Spacing size={16} />
+                            {showFriendsList ? (
+                                <FriendList />
+                            ) : (
+                                <Button onClick={toggleShowFriendsList}>
+                                    새로운 쪽지를 보내보세요!
+                                </Button>
+                            )}
+                        </>
+                    ) : (
+                        <>
+                            <Text>
+                                아직 쪽지를 사용하지 않았어요. 날 선택한
+                                친구에게 쪽지를 보내보세요!
+                            </Text>
+                            <FriendList />
+                        </>
+                    )}
                 </ChattingWrapper>
             </ChatWrapper>
             <BottomNavigation />
@@ -29,26 +68,3 @@ const ChattingWrapper = styled.div`
     flex-direction: column;
     gap: 24px;
 `;
-
-const chats = [
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    ""
-];
---
+++ src/app/chatting/[id]/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+import BackTitle from "src/pages-edit/chatting/BackTitle";
+import ChattingBox from "src/pages-edit/chatting/ChattingBox";
+import Question from "src/pages-edit/chatting/Question";
+import styled from "styled-components";
+import { useGetChatroomChats } from "src/hooks/chatting/useGetChatroomId";
+import Text from "src/components/common/Text";
+import Input from "src/components/Input/Input";
+import { postChatroomChats } from "src/apis/chatting";
+import { pipe } from "fp-ts/lib/function";
+
+export default function Page({ params }: { params: { id: number } }) {
+    const chatroomId = params.id;
+    const {
+        data: chatroomData,
+        isLoading,
+        refetch
+    } = useGetChatroomChats({
+        chatroomId: params.id
+    });
+
+    const sendMessage = (value: string) => {
+        postChatroomChats(chatroomId, {
+            message: value
+        });
+        refetch(); // not working?
+    };
+
+    return isLoading ? undefined : (
+        <div style={{ height: "100vh" }}>
+            <BackTitle {...chatroomData} />
+            <PageWrapper>
+                {/* <BackTitle {...chatroomData.roomDetails} /> */}
+                <Question />
+
+                <ChattingWrapper>
+                    {chatroomData?.count ? (
+                        [...chatroomData?.data]
+                            .reverse()
+                            .map((data: any, idx: number) => {
+                                return <ChattingBox key={idx} {...data} />;
+                            })
+                    ) : (
+                        <Text>메세지 없음</Text>
+                    )}
+                </ChattingWrapper>
+
+                <Input
+                    type="chatting-send"
+                    limit={0}
+                    onIconPress={sendMessage}
+                />
+            </PageWrapper>
+        </div>
+    );
+}
+
+const PageWrapper = styled.div`
+    position: relative;
+    width: 100%;
+    height: calc(100% - 6rem);
+    padding: 0 16px;
+    display: flex;
+    overflow: hidden;
+    gap: 1rem;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+const ChattingWrapper = styled.div`
+    overflow-y: auto;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+
+    &::-webkit-scrollbar {
+        width: 0.1rem;
+    }
+
+    &::-webkit-scrollbar-track {
+        background: transparent;
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background: transparent;
+    }
+`;
