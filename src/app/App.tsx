463c36ab5cac27455d6584069e7bb1de0cbd3e56
hamo-o <yheel369@gmail.com>
2023-09-03 14:16:58 +0900

    feat: ios zoom 막기

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,6 +7,13 @@ import BottomSheet from "src/components/BottomSheet";
 export default function App({ children }: { children: React.ReactNode }) {
     const { visible } = useAppSelector((state) => state.bottomSheet);
 
+    const preventZoom = (event: TouchEvent) => {
+        if (event.touches.length > 1) {
+            event.preventDefault();
+        }
+    };
+    document.addEventListener("touchmove", preventZoom, { passive: false });
+
     return (
         <StyledComponentsRegistry>
             <>
