24a193dd3d0745a101484d18f9163a2fb0067205
cyeju <yeju1019@gmail.com>
2023-09-05 21:10:38 +0900

    fix: apple login script 설정

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export default function Head() {
     return (
         <head>
@@ -10,6 +12,10 @@ export default function Head() {
                 rel="stylesheet"
                 type="text/css"
                 href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard-dynamic-subset.css"
+            />
+            <Script
+                src="https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js"
+                type="text/javascript"
             />
         </head>
     );
