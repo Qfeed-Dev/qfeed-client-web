3ebcefa0ca94bb1906218e4c5d9bd409e82b3fd7
hamo-o <yheel369@gmail.com>
2023-09-07 00:28:52 +0900

    fix: 회원가입 첫 페이지 백버튼 라우팅 변경

--- src/pages-edit/sign-up/default.tsx
+++ src/pages-edit/sign-up/default.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import ButtonFillLarge from "src/components/buttons/button-fill-large";
 import Flex from "src/components/common/Flex";
 import InputLine from "src/components/inputs/input-line";
-import NavigationTop from "src/components/navigations/NavigationTopBack";
+import NavigationTop from "src/components/navigations/NavigationTop";
 import ButtonGenderSelect from "src/components/sign-up/button-gender-select";
 import { birthMsg, emailMsg, nameMsg, phoneMsg } from "src/constants/messages";
 import { useCheckNicknameQuery } from "src/hooks/account/useCheckNicknameQuery";
@@ -63,7 +63,12 @@ const SignIn = () => {
     return (
         <Flex direction="column" justify="start" gap={24}>
             <NavigationTop
-                leftIcon={<Icon icon="LeftArrow" onClick={router.back} />}
+                leftIcon={
+                    <Icon
+                        icon="LeftArrow"
+                        onClick={() => router.push("/account")}
+                    />
+                }
                 title="회원 가입"
             />
             <Flex direction="column" justify="start" gap={24}>
@@ -71,7 +76,7 @@ const SignIn = () => {
                     value={user?.name || name.value}
                     onChange={name.handleChangeInput}
                     label="이름"
-                    placeholder="ex) 황채린"
+                    placeholder="ex) 홍길동"
                     message={nameMsg.RIGHT}
                     readonly={Boolean(user?.name)}
                 />
@@ -83,7 +88,7 @@ const SignIn = () => {
                     value={user?.birthday?.split("T")[0] || birthday.value}
                     onChange={birthday.handleChangeInput}
                     label="생년월일"
-                    placeholder="ex) 1997-04-02"
+                    placeholder="ex) 2005-01-01"
                     message={
                         user?.birthday
                             ? undefined
@@ -98,7 +103,7 @@ const SignIn = () => {
                     value={user?.phone || phone.value}
                     onChange={phone.handleChangeInput}
                     label="휴대폰 번호"
-                    placeholder="ex) 01050165886"
+                    placeholder="ex) 01012345678"
                     message={
                         validPhone(phone.value)
                             ? phoneMsg.RIGHT
@@ -111,7 +116,7 @@ const SignIn = () => {
                     value={user?.email || email.value}
                     onChange={email.handleChangeInput}
                     label="이메일"
-                    placeholder="ex) ghkdcofls42@naver.com"
+                    placeholder="ex) qfeed@naver.com"
                     message={
                         user?.email
                             ? undefined
@@ -127,7 +132,7 @@ const SignIn = () => {
                         value={user?.nickname || nickname.value}
                         onChange={nickname.handleChangeInput}
                         label="닉네임"
-                        placeholder="ex) qwerk11"
+                        placeholder="ex) qfeed"
                         message={
                             user?.nickname
                                 ? undefined
