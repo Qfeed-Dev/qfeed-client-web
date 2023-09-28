3dc088cd365105ad355c621fa81d147f2cde0664
hamo-o <yheel369@gmail.com>
2023-09-28 15:25:13 +0900

    feat: 유저 차단 모달 완성

--- src/app/(padding)/friend/@modal/ban/page.tsx
+++ src/app/(padding)/friend/@modal/ban/page.tsx
@@ -10,7 +10,7 @@ export default function AccountCheckModal() {
         <Modal
             handleClose={() => router.back()}
             title="차단하시겠어요?"
-            detail="쪽지 수신 및 발신이 모두 차단되며, 다시 해제하실 수 없습니다."
+            detail="쪽지 수신 및 발신이 모두 차단되며,\n다시 해제하실 수 없습니다."
             rightText="차단"
             rightClick={() => {}}
         />
