bb96bd5582442664c841b361ebedec63d0cf1ace
hamo-o <yheel369@gmail.com>
2023-10-15 10:39:40 +0900

    fix: 문제수 UI 텍스트 수정

--- src/pages-edit/home/components/MakeOfficial.tsx
+++ src/pages-edit/home/components/MakeOfficial.tsx
@@ -140,11 +140,11 @@ const MakeOfficial = (props: QuestionProps) => {
                                 {QSet.questionCursor[0].currentQ}
                             </Text>
                             <Text typo="Caption1r" color="light_qblack">
-                                아직
-                                {QSet.questionCursor[0].QsetLength -
-                                    QSet.questionCursor[0].cursor +
-                                    1}
-                                문제 남았어요!
+                                <span>아직 </span>
+                                <span>
+                                    {10 - QSet.questionCursor[0].cursor + 1}
+                                    문제 남았어요!
+                                </span>
                             </Text>
                             <BottomButton>
                                 <Text
